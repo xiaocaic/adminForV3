201588d02c261ceeb776c0a6753aff1cde9bcb35
cyh <841561248@qq.com>
2022-07-08 15:30:21 +0800

    定义type

--- src/views/about/index.vue
+++ src/views/about/index.vue
@@ -1,28 +1,28 @@
 <template>
-  <el-form ref="form" :model="form" label-width="120px">
+  <el-form ref="form" :model="data.form" label-width="120px">
     <el-form-item label="Activity name">
-      <el-input v-model="form.name"></el-input>
+      <el-input v-model="data.form.name"></el-input>
     </el-form-item>
     <el-form-item label="Activity zone">
-      <el-select v-model="form.region" placeholder="please select your zone">
+      <el-select v-model="data.form.region" placeholder="please select your zone">
         <el-option label="Zone one" value="shanghai"></el-option>
         <el-option label="Zone two" value="beijing"></el-option>
       </el-select>
     </el-form-item>
     <el-form-item label="Activity time">
       <el-col :span="11">
-        <el-date-picker v-model="form.date1" type="date" placeholder="Pick a date" style="width: 100%"></el-date-picker>
+        <el-date-picker v-model="data.form.date1" type="date" placeholder="Pick a date" style="width: 100%"></el-date-picker>
       </el-col>
       <el-col class="line" :span="2">-</el-col>
       <el-col :span="11">
-        <el-time-picker v-model="form.date2" placeholder="Pick a time" style="width: 100%"></el-time-picker>
+        <el-time-picker v-model="data.form.date2" placeholder="Pick a time" style="width: 100%"></el-time-picker>
       </el-col>
     </el-form-item>
     <el-form-item label="Instant delivery">
-      <el-switch v-model="form.delivery"></el-switch>
+      <el-switch v-model="data.form.delivery"></el-switch>
     </el-form-item>
     <el-form-item label="Activity type">
-      <el-checkbox-group v-model="form.type">
+      <el-checkbox-group v-model="data.form.type">
         <el-checkbox label="Online activities" name="type"></el-checkbox>
         <el-checkbox label="Promotion activities" name="type"></el-checkbox>
         <el-checkbox label="Offline activities" name="type"></el-checkbox>
@@ -30,13 +30,13 @@
       </el-checkbox-group>
     </el-form-item>
     <el-form-item label="Resources">
-      <el-radio-group v-model="form.resource">
+      <el-radio-group v-model="data.form.resource">
         <el-radio label="Sponsor"></el-radio>
         <el-radio label="Venue"></el-radio>
       </el-radio-group>
     </el-form-item>
     <el-form-item label="Activity form">
-      <el-input v-model="form.desc" type="textarea"></el-input>
+      <el-input v-model="data.form.desc" type="textarea"></el-input>
     </el-form-item>
     <el-form-item>
       <el-button type="primary" @click="onSubmit">Create</el-button>
@@ -45,26 +45,25 @@
   </el-form>
 </template>
 
-<script lang="ts">
-export default {
-  data() {
-    return {
-      form: {
-        name: '',
-        region: '',
-        date1: '',
-        date2: '',
-        delivery: false,
-        type: [],
-        resource: '',
-        desc: '',
-      },
-    };
+<script setup lang="ts">
+import { reactive } from '@vue/reactivity';
+import { type } from 'os';
+type dataTypes = {
+  form: Object;
+};
+const data: dataTypes = reactive({
+  form: {
+    name: '',
+    region: '',
+    date1: '',
+    date2: '',
+    delivery: false,
+    type: [],
+    resource: '',
+    desc: '',
   },
-  methods: {
-    onSubmit() {
-      console.log('submit!');
-    },
-  },
+});
+const onSubmit = (): any => {
+  console.log('submit!');
 };
 </script>
--- src/views/table/index.vue
+++ src/views/table/index.vue
@@ -1,5 +1,5 @@
 <template>
-  <el-table :data="tableData" style="width: 100%">
+  <el-table :data="data.tableData" style="width: 100%">
     <el-table-column prop="date" label="Date" width="150" />
     <el-table-column label="Delivery Info">
       <el-table-column prop="name" label="Name" width="120" />
@@ -13,69 +13,77 @@
   </el-table>
 </template>
 
-<script lang="ts">
-export default {
-  data() {
-    return {
-      tableData: [
-        {
-          date: '2016-05-03',
-          name: 'Tom',
-          state: 'California',
-          city: 'Los Angeles',
-          address: 'No. 189, Grove St, Los Angeles',
-          zip: 'CA 90036',
-        },
-        {
-          date: '2016-05-02',
-          name: 'Tom',
-          state: 'California',
-          city: 'Los Angeles',
-          address: 'No. 189, Grove St, Los Angeles',
-          zip: 'CA 90036',
-        },
-        {
-          date: '2016-05-04',
-          name: 'Tom',
-          state: 'California',
-          city: 'Los Angeles',
-          address: 'No. 189, Grove St, Los Angeles',
-          zip: 'CA 90036',
-        },
-        {
-          date: '2016-05-01',
-          name: 'Tom',
-          state: 'California',
-          city: 'Los Angeles',
-          address: 'No. 189, Grove St, Los Angeles',
-          zip: 'CA 90036',
-        },
-        {
-          date: '2016-05-08',
-          name: 'Tom',
-          state: 'California',
-          city: 'Los Angeles',
-          address: 'No. 189, Grove St, Los Angeles',
-          zip: 'CA 90036',
-        },
-        {
-          date: '2016-05-06',
-          name: 'Tom',
-          state: 'California',
-          city: 'Los Angeles',
-          address: 'No. 189, Grove St, Los Angeles',
-          zip: 'CA 90036',
-        },
-        {
-          date: '2016-05-07',
-          name: 'Tom',
-          state: 'California',
-          city: 'Los Angeles',
-          address: 'No. 189, Grove St, Los Angeles',
-          zip: 'CA 90036',
-        },
-      ],
-    }
+<script setup lang="ts">
+import { reactive } from '@vue/reactivity';
+type dataInterface = {
+  tableData: {
+    date: String;
+    name: String;
+    state: String;
+    city: String;
+    address: String;
+    zip: String;
+  }[];
+};
+let data: dataInterface = reactive({
+  tableData: [],
+});
+data.tableData = [
+  {
+    date: '2016-05-03',
+    name: 'Tom',
+    state: 'California',
+    city: 'Los Angeles',
+    address: 'No. 189, Grove St, Los Angeles',
+    zip: 'CA 90036',
   },
-}
+  {
+    date: '2016-05-02',
+    name: 'Tom',
+    state: 'California',
+    city: 'Los Angeles',
+    address: 'No. 189, Grove St, Los Angeles',
+    zip: 'CA 90036',
+  },
+  {
+    date: '2016-05-04',
+    name: 'Tom',
+    state: 'California',
+    city: 'Los Angeles',
+    address: 'No. 189, Grove St, Los Angeles',
+    zip: 'CA 90036',
+  },
+  {
+    date: '2016-05-01',
+    name: 'Tom',
+    state: 'California',
+    city: 'Los Angeles',
+    address: 'No. 189, Grove St, Los Angeles',
+    zip: 'CA 90036',
+  },
+  {
+    date: '2016-05-08',
+    name: 'Tom',
+    state: 'California',
+    city: 'Los Angeles',
+    address: 'No. 189, Grove St, Los Angeles',
+    zip: 'CA 90036',
+  },
+  {
+    date: '2016-05-06',
+    name: 'Tom',
+    state: 'California',
+    city: 'Los Angeles',
+    address: 'No. 189, Grove St, Los Angeles',
+    zip: 'CA 90036',
+  },
+  {
+    date: '2016-05-07',
+    name: 'Tom',
+    state: 'California',
+    city: 'Los Angeles',
+    address: 'No. 189, Grove St, Los Angeles',
+    zip: 'CA 90036',
+  },
+];
 </script>
